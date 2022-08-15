8da1ba964e08cca5318cd2da0c5dbc6079cf9419
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-08-15 11:01:07 -0700

    team page
    added twitter buttons, titles, and slight mobile support changes.

--- components/Team.tsx
+++ components/Team.tsx
@@ -1,27 +1,58 @@
+import { BsTwitter } from "react-icons/bs";
+
 const Team = () => {
     return (
         <div className="flex justify-center h-screen bg-gray-400 z-[1]" id="Team">
           <img className="object-cover lg:object-fill h-full w-full absolute z-[2]" src="bg4.png"></img>
-          <div className="m-auto z-[4] grid grid-cols-1 lg:grid-cols-5 gap-10">
-            <div className="bg-teal-500 p-10 drop-shadow-[15px_5px_10px_rgba(0,0,0,0.4)]">
-              <div className="text-center font-bold text-3xl mb-10">Dody</div>
-              <img className="w-[200px]" src="dody.png"></img>
+          <div className="m-auto z-[4] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10">
+            <div>
+              <div className="bg-teal-500 px-10 drop-shadow-[15px_5px_10px_rgba(0,0,0,0.4)]">
+                <div className="text-center font-bold text-3xl py-5">Dody</div>
+                <img className="w-[200px]" src="dody.png"></img>
+                <div className="text-center text-xl my-2">Owner</div>
+                <div>
+                  <a href="https://twitter.com/HeyDody">
+                    <BsTwitter className="m-auto mb-5"/>
+                  </a>
+                </div>
+              </div>
             </div>
-            <div className="bg-teal-500 p-10 drop-shadow-[15px_5px_10px_rgba(0,0,0,0.4)]">
-              <div className="text-center font-bold text-3xl mb-10">Dylan</div>
-              <img className="w-[200px]" src="dylan.png"></img>
+            <div>
+              <div className="bg-teal-500 px-10 drop-shadow-[15px_5px_10px_rgba(0,0,0,0.4)]">
+                <div className="text-center font-bold text-3xl py-5">Dylan</div>
+                <img className="w-[200px]" src="dylan.png"></img>
+                <div className="text-center text-xl my-2">Alpha Caller</div>
+                  <a href="https://twitter.com/0x_Dylan">
+                    <BsTwitter className="m-auto mb-5"/>
+                  </a>
+              </div>
             </div>
-            <div className="bg-teal-500 p-10 drop-shadow-[15px_5px_10px_rgba(0,0,0,0.4)]">
-              <div className="text-center font-bold text-3xl mb-10">Yasha</div>
-              <img className="w-[200px]" src="yasha.png"></img>
+            <div>
+              <div className="bg-teal-500 px-10 drop-shadow-[15px_5px_10px_rgba(0,0,0,0.4)]">
+                <div className="text-center font-bold text-3xl py-5">Yasha</div>
+                <img className="w-[200px]" src="yasha.png"></img>
+                <div className="text-center text-xl my-2">Alpha Caller</div>
+              </div>
             </div>
-            <div className="bg-teal-500 p-10 drop-shadow-[15px_5px_10px_rgba(0,0,0,0.4)]">
-              <div className="text-center font-bold text-3xl mb-10">Chiefnquach</div>
-              <img className="w-[200px]" src="chiefnquach.gif"></img>
+            <div>
+              <div className="bg-teal-500 px-10 drop-shadow-[15px_5px_10px_rgba(0,0,0,0.4)]">
+                <div className="text-center font-bold text-3xl py-5">Chiefnquach</div>
+                <img className="w-[200px]" src="chiefnquach.gif"></img>
+                <div className="text-center text-xl my-2">Collab Manager</div>
+                  <a href="https://twitter.com/chiefnquach">
+                    <BsTwitter className="m-auto mb-5"/>
+                  </a>
+              </div>
             </div>
-            <div className="bg-teal-500 p-10 drop-shadow-[15px_5px_10px_rgba(0,0,0,0.4)]">
-              <div className="text-center font-bold text-3xl mb-10">captinturtle</div>
-              <img className="w-[200px]" src="captinturtle.png"></img>
+            <div>
+              <div className="bg-teal-500 px-10 drop-shadow-[15px_5px_10px_rgba(0,0,0,0.4)] col-span-2 lg:col-span-1">
+                <div className="text-center font-bold text-3xl py-5">Captinturtle</div>
+                <img className="w-[200px]" src="captinturtle.png"></img>
+                <div className="text-center text-xl py-2">Dev</div>
+                  <a href="https://twitter.com/foxninjac71">
+                    <BsTwitter className="m-auto"/>
+                  </a>
+              </div>
             </div>
           </div>
         </div>
