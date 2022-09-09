b2542ba71a580c84c4fa02e4eb0f81d23a453186
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-09-08 21:50:34 -0700

    about, landing
    text size changes

--- components/About.tsx
+++ components/About.tsx
@@ -6,19 +6,19 @@ const About = () => {
             <div className="grid grid-cols-2 gap-4 lg:gap-5 m-4 text-white">
               <div className=" bg-slate-800 rounded-3xl min-h-[300px] md:min-h-0 drop-shadow-[5px_10px_10px_rgba(0,0,0,0.4)] transition-all hover:translate-x-[1px] hover:translate-y-[1px] hover:drop-shadow-[5px_6px_6px_rgba(0,0,0,0.4)]">
                 <div className="text-[1rem] lg:text-lg font-bold p-8 drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]">A PRIME NFT PASS IS</div>
-                <div className="max-w-[300px] text-[90%] xl:text-[1.15rem] xl:text-xl px-8 h-40">By activating your squid pass you are gaining access to in depth trading platform with a community first</div>
+                <div className="max-w-[300px] text-[90%] xl:text-[1.1rem] xl:text-xl px-8 h-40">By activating your squid pass you are gaining access to in depth trading platform with a community first</div>
               </div>
               <div className=" bg-slate-800 rounded-3xl min-h-[300px] md:min-h-0 drop-shadow-[5px_10px_10px_rgba(0,0,0,0.4)] transition-all hover:translate-x-[1px] hover:translate-y-[1px] hover:drop-shadow-[5px_6px_6px_rgba(0,0,0,0.4)]">
                 <div className="text-[1rem] lg:text-lg font-bold p-8 drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]">A PRIME NFT PASS IS</div>
-                <div className="max-w-[300px] text-[90%] xl:text-[1.15rem] xl:text-xl px-8 h-40">By activating your squid pass you are gaining access to in depth trading platform with a community first</div>
+                <div className="max-w-[300px] text-[90%] xl:text-[1.1rem] xl:text-xl px-8 h-40">By activating your squid pass you are gaining access to in depth trading platform with a community first</div>
               </div>
               <div className=" bg-slate-800 rounded-3xl min-h-[300px] md:min-h-0 drop-shadow-[5px_10px_10px_rgba(0,0,0,0.4)] transition-all hover:translate-x-[1px] hover:translate-y-[1px] hover:drop-shadow-[5px_6px_6px_rgba(0,0,0,0.4)]">
                 <div className="text-[1rem] lg:text-lg font-bold p-8 drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]">A PRIME NFT PASS IS</div>
-                <div className="max-w-[300px] text-[90%] xl:text-[1.15rem] xl:text-xl px-8 h-40">By activating your squid pass you are gaining access to in depth trading platform with a community first</div>
+                <div className="max-w-[300px] text-[90%] xl:text-[1.1rem] xl:text-xl px-8 h-40">By activating your squid pass you are gaining access to in depth trading platform with a community first</div>
               </div>
               <div className=" bg-slate-800 rounded-3xl min-h-[300px] md:min-h-0 drop-shadow-[5px_10px_10px_rgba(0,0,0,0.4)] transition-all hover:translate-x-[1px] hover:translate-y-[1px] hover:drop-shadow-[5px_6px_6px_rgba(0,0,0,0.4)]">
                 <div className="text-[1rem] lg:text-lg font-bold p-8 drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]">A PRIME NFT PASS IS</div>
-                <div className="max-w-[300px] text-[90%] xl:text-[1.15rem] xl:text-xl px-8 h-40">By activating your squid pass you are gaining access to in depth trading platform with a community first</div>
+                <div className="max-w-[300px] text-[90%] xl:text-[1.1rem] xl:text-xl px-8 h-40">By activating your squid pass you are gaining access to in depth trading platform with a community first</div>
               </div>
             </div>
             <div className="flex justify-center">
