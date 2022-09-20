8954b22ecded0759a7af708b164954b6261b07ad
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-09-20 00:15:38 -0700

    tools
    added semi functioning swiper carrousel

--- components/Tools.tsx
+++ components/Tools.tsx
@@ -6,12 +6,22 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image'
 import bg2 from '../public/bg2.png'
 
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+
 
 
 // 0 = image, 1 = title, 2 = desc
 const nftnerdsDesc: any = [nftnerdsExample, "NFTNerds.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."];
 const zetaDesc: any = [zetaExample, "Zeta.", "Aliquam faucibus purus in massa tempor. Aenean sed adipiscing diam donec adipiscing tristique risus. Velit egestas dui id ornare."];
 const degenmintDesc: any = [degenmintExample, "Degen Mint.", "Nullam ac tortor vitae purus faucibus ornare suspendisse. Risus pretium quam vulputate dignissim suspendisse in est."];
+
+const SwiperButtonNext = ({ children }) => {
+  const swiper = useSwiper();
+
+  return (
+    <button onClick={() => swiper.slideNext()}>A</button>
+  );
+};
 
 const Tools = () => {
   const [example, setExample] = useState(nftnerdsDesc);
@@ -46,6 +56,8 @@ const Tools = () => {
     updateImage();
   };
 
+  const swiper = useSwiper();
+
     return (
         <div className="grid justify-center h-[1000px] lg:h-[900px] bg-gray-200 z-[1]" id="Tools">
           <div className="object-cover lg:object-fill h-[1001px] lg:h-[901px] w-full absolute z-[2]">
@@ -53,21 +65,17 @@ const Tools = () => {
           </div>
           <div className="mx-8 z-[4] grid grid-cols-1 lg:grid-cols-2 gap-10 text-white mt-auto">
             <div className="bg-slate-800 m-auto p-10 rounded-3xl max-w-[500px] lg:min-h-0 drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]">
-              <div className={exampleCount === 0 ? "transition-all" : "transition-all w-0 h-0 translate-x-10"}>
-                <Image src={nftnerdsExample}/>
-              </div>
-              <div className={exampleCount === 1 ? "transition-all" : "transition-all w-0 h-0 translate-x-10"}>
-                <Image src={zetaExample}/>
-              </div>
-              <div className={exampleCount === 2 ? "transition-all" : "transition-all w-0 h-0 translate-x-10"}>
-                <Image src={degenmintExample}/>
-              </div>
-              <div className="pt-5 font-bold">{example[1]}</div>
-              <div className="pt-5 max-w-lg min-h-[100px]">{example[2]}</div>
-            </div>
-            <div className="flex lg:hidden justify-center text-white z-[4] gap-20 text-[30px] select-none pb-10">
-              <AiOutlineLeft onClick={cycleDown} className="hover:text-gray-300 cursor-pointer"/>
-              <AiOutlineRight onClick={cycleUp} className="hover:text-gray-300 cursor-pointer"/>
+              <Swiper 
+                spaceBetween={150}
+                loop={true}
+                className="w-96 h-96 rounded-xl"
+              >
+                <SwiperSlide className="text-center text-5xl flex bg-green-200 rounded-xl"><div className="m-auto">Slide 1</div></SwiperSlide>
+                <SwiperSlide className="text-center text-5xl flex bg-green-200 rounded-xl"><div className="m-auto">Slide 2</div></SwiperSlide>
+                <SwiperSlide className="text-center text-5xl flex bg-green-200 rounded-xl"><div className="m-auto">Slide 3</div></SwiperSlide>
+                <SwiperSlide className="text-center text-5xl flex bg-green-200 rounded-xl"><div className="m-auto">Slide 4</div></SwiperSlide>
+                <SwiperSlide className="text-center text-5xl flex bg-green-200 rounded-xl"><div className="m-auto">Slide 5</div></SwiperSlide>
+              </Swiper>
             </div>
             <div className="m-auto text-left">
               <div className="text-3xl lg:text-5xl font-bold pb-3 md:m-0 drop-shadow-[15px_5px_10px_rgba(0,0,0,0.4)] flex justify-center">Keys to our success</div>
@@ -75,7 +83,9 @@ const Tools = () => {
               <div className="max-w-[600px] mx-[20px] text-[95%] lg:text-[115%] py-5 px-10 select-none rounded-3xl text-center lg:text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
             </div>
             <div className="hidden lg:flex justify-center text-white z-[4] gap-20 text-[30px] select-none">
-              <AiOutlineLeft onClick={cycleDown} className="hover:text-gray-300 cursor-pointer"/>
+              <SwiperButtonNext>
+                <AiOutlineLeft onClick={cycleDown} className="hover:text-gray-300 cursor-pointer"/>
+              </SwiperButtonNext>
               <AiOutlineRight onClick={cycleUp} className="hover:text-gray-300 cursor-pointer"/>
             </div>
           </div>
