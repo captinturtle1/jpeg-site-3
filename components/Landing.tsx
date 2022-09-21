31ed35730e4389417aad77ebdce4e2fb8ca32858
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-09-20 23:47:56 -0700

    mobile support
    Updated landing and tools page to support smaller screen sizes

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -5,26 +5,28 @@ import squid from '../public/passText.png'
 
 const Landing = () => {
   return (
-    <div className="flex relative justify-center h-[970px] bg-gray-900" id="Landing">
+    <div className="flex relative justify-center h-[1200px] lg:h-[970px] bg-gray-900" id="Landing">
       <div className="bottom-0 absolute z-[2] translate-y-[6.9px]">
         <Image quality={100} src={bg}/>
       </div>
       <div className="z-[4] grid grid-cols-1 h-[900px] lg:grid-cols-2 gap-20">
-        <div className="m-auto w-[650px] flex justify-center drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]">
+        <div className="mx-auto mt-32 lg:m-auto w-[200px] xs:w-[300px] lg:w-[650px] flex justify-center drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]">
           <div>
             <Image src={squid}/>
           </div>
         </div>
-        <div className="m-auto">
-          <div className="text-7xl font-bold pb-8">Into the abyss</div>
-          <div className="max-w-[700px] text-2xl pb-8">A private collective of skilled traders and investors navigating the web3 space. Membership passes grant access to all community benefits while active. Each membership runs on a 30 day renewal system and can be managed at anytime via our dashboard.</div>
-          <div className="max-w-[700px] text-2xl pb-8">Membership passes grant access to all community benefits while active. Each membership runs on a 30 day renewal system and can be managed at anytime via our dashboard.</div>
-          <a>
-            <button className="transition-all inline-block text-2xl px-6 py-3 leading-none rounded font-bold text-white bg-blue-500 hover:bg-blue-400 drop-shadow hover:drop-shadow-sm">Apply</button>
-          </a>
-          <a href="https://opensea.io/collection/abyssfnf">
-            <button className="ml-6 transition-all inline-block text-2xl px-6 py-3 mb-3 leading-none rounded font-bold text-white bg-blue-500 hover:bg-blue-400 drop-shadow hover:drop-shadow-sm">Buy Pass</button>
-          </a>
+        <div className="m-auto px-10 lg:p-0 flex flex-wrap">
+          <div className="text-5xl lg:text-7xl text-center lg:text-left font-bold pb-8">Into the abyss</div>
+          <div className="max-w-[700px] text-center lg:text-left text-md xs:text-xl lg:text-2xl pb-8">A private collective of skilled traders and investors navigating the web3 space. Membership passes grant access to all community benefits while active. Each membership runs on a 30 day renewal system and can be managed at anytime via our dashboard.</div>
+          <div className="max-w-[700px] text-center lg:text-left text-md xs:text-xl lg:text-2xl pb-8">Membership passes grant access to all community benefits while active. Each membership runs on a 30 day renewal system and can be managed at anytime via our dashboard.</div>
+          <div className="grid xs:block gap-5 xs:gap-0 m-auto lg:m-0">
+            <a className="m-auto">
+              <button className="transition-all inline-block text-2xl px-6 py-3 leading-none rounded font-bold text-white bg-blue-500 hover:bg-blue-400 drop-shadow hover:drop-shadow-sm">Apply</button>
+            </a>
+            <a href="https://opensea.io/collection/abyssfnf" className="m-auto">
+              <button className="xs:ml-6 transition-all inline-block text-2xl px-6 py-3 mb-3 leading-none rounded font-bold text-white bg-blue-500 hover:bg-blue-400 drop-shadow hover:drop-shadow-sm">Buy Pass</button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- components/Tools.tsx
+++ components/Tools.tsx
@@ -15,6 +15,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 const Tools = () => {
   const [swiperRef, setSwiperRef] = useState<any>();
+  const [swiperRefMobile, setSwiperRefMobile] = useState<any>();
 
   const handleLeftClick = useCallback(() => {
     if (!swiperRef) return;
@@ -25,96 +26,193 @@ const Tools = () => {
     if (!swiperRef) return;
     swiperRef.slideNext();
   }, [swiperRef]);
+
+  const handleLeftClickMobile = useCallback(() => {
+    if (!swiperRefMobile) return;
+    swiperRefMobile.slidePrev();
+  }, [swiperRefMobile]);
+
+  const handleRightClickMobile = useCallback(() => {
+    if (!swiperRefMobile) return;
+    swiperRefMobile.slideNext();
+  }, [swiperRefMobile]);
+
     return (
         <div className="grid justify-center h-[100vh] bg-gray-200 z-[1]" id="Tools">
           <div className="object-cover lg:object-fill h-[100vh] w-full absolute z-[2]">
             <Image layout="fill" quality={100} src={bg}/>
           </div>
-          <div className="z-[4] flex">
-              <div className="m-auto">
-                <div className="text-5xl text-white hover:text-gray-300 cursor-pointer" onClick={handleLeftClick}>
-                  <FiChevronLeft/>
-                </div>
+          <div className="z-[4] hidden lg:flex">
+            <div className="m-auto">
+              <div className="text-5xl text-white hover:text-gray-300 cursor-pointer" onClick={handleLeftClick}>
+                <FiChevronLeft/>
               </div>
-              <Swiper
-                onSwiper={setSwiperRef}
-                spaceBetween={100}
-                loop={true}
-                slidesPerView={3}
-                className="w-[1350px] rounded-3xl select-none m-auto drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]"
-              >
-                <SwiperSlide>
-                  <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
-                    <div className="flex-grow text-5xl rounded-t-3xl m-auto">
-                      <Image src={fomoAlert} className="rounded-3xl"/>
-                    </div>
-                    <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">FOMO Alerts</div>
-                      <div className="text-[1rem] h-16">FOMO alerts are extremely useful and will notify you based on different market conditions surrounding trending projects.</div>
-                    </div>
+            </div>
+            <Swiper
+              onSwiper={setSwiperRef}
+              spaceBetween={100}
+              loop={true}
+              slidesPerView={3}
+              className="w-[1350px] rounded-3xl select-none m-auto drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]"
+            >
+              <SwiperSlide>
+                <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={fomoAlert} className="rounded-3xl"/>
                   </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
-                    <div className="flex-grow text-5xl rounded-t-3xl m-auto">
-                      <Image src={followAlerts} className="rounded-3xl"/>
-                    </div>
-                    <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">Follow Alerts</div>
-                      <div className="text-[1rem] h-16">Follow alerts will show you who important figures are following and can be useful for finding new projects before they blow up.</div>
-                    </div>
+                  <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">FOMO Alerts</div>
+                    <div className="text-[1rem] h-16">FOMO alerts are extremely useful and will notify you based on different market conditions surrounding trending projects.</div>
                   </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
-                    <div className="flex-grow text-5xl rounded-t-3xl m-auto">
-                      <Image src={contractScraper} className="rounded-3xl"/>
-                    </div>
-                    <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">Contract Alerts</div>
-                      <div className="text-[1rem] h-16">Contract alerts makes sure you have access to any lowkey mints or stealth drops that fly under the radar since not every mint is announced.</div>
-                    </div>
-                  </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
-                    <div className="flex-grow text-5xl rounded-t-3xl m-auto">
-                      <Image src={mintScraper} className="rounded-3xl"/>
-                    </div>
-                    <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">Mint Alerts</div>
-                      <div className="text-[1rem] h-16">Mint alerts will let you know when a new project has begun minting or when and older project gains some traction and begins to sell out.</div>
-                    </div>
-                  </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
-                    <div className="flex-grow text-5xl rounded-t-3xl m-auto">
-                      <Image src={raffleScraper} className="rounded-3xl"/>
-                    </div>
-                    <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">Raffle Alerts</div>
-                      <div className="text-[1rem] h-16">Raffle alerts will ensure you never miss another whitelist opportunity whether its on premint, superful, discord, twitter, you name it.</div>
-                    </div>
-                  </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
-                    <div className="flex-grow text-5xl rounded-t-3xl m-auto">
-                      <Image src={calander} className="rounded-3xl"/>
-                    </div>
-                    <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">Daily Calendar</div>
-                      <div className="text-[1rem] h-16">A daily list that is meant to serve as a quick guide for new projects minting from day to day and is updated regularly.</div>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              </Swiper>
-              <div className="m-auto">
-                <div className="text-5xl text-white hover:text-gray-300 cursor-pointer" onClick={handleRightClick}>
-                  <FiChevronRight/>
                 </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={followAlerts} className="rounded-3xl"/>
+                  </div>
+                  <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">Follow Alerts</div>
+                    <div className="text-[1rem] h-16">Follow alerts will show you who important figures are following and can be useful for finding new projects before they blow up.</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={contractScraper} className="rounded-3xl"/>
+                  </div>
+                  <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">Contract Alerts</div>
+                    <div className="text-[1rem] h-16">Contract alerts makes sure you have access to any lowkey mints or stealth drops that fly under the radar since not every mint is announced.</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={mintScraper} className="rounded-3xl"/>
+                  </div>
+                  <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">Mint Alerts</div>
+                    <div className="text-[1rem] h-16">Mint alerts will let you know when a new project has begun minting or when and older project gains some traction and begins to sell out.</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={raffleScraper} className="rounded-3xl"/>
+                  </div>
+                  <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">Raffle Alerts</div>
+                    <div className="text-[1rem] h-16">Raffle alerts will ensure you never miss another whitelist opportunity whether its on premint, superful, discord, twitter, you name it.</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="bg-slate-800 w-96 rounded-3xl flex flex-col pt-7">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={calander} className="rounded-3xl"/>
+                  </div>
+                  <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">Daily Calendar</div>
+                    <div className="text-[1rem] h-16">A daily list that is meant to serve as a quick guide for new projects minting from day to day and is updated regularly.</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+            <div className="m-auto">
+              <div className="text-5xl text-white hover:text-gray-300 cursor-pointer" onClick={handleRightClick}>
+                <FiChevronRight/>
               </div>
+            </div>
+          </div>
+          <div className="z-[4] flex lg:hidden">
+            <div className="m-auto">
+              <div className="hidden xs:block text-5xl text-white hover:text-gray-300 cursor-pointer" onClick={handleLeftClickMobile}>
+                <FiChevronLeft/>
+              </div>
+            </div>
+            <Swiper
+              onSwiper={setSwiperRefMobile}
+              spaceBetween={150}
+              loop={true}
+              slidesPerView={1}
+              className="w-[280px] rounded-3xl select-none m-auto drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]"
+            >
+              <SwiperSlide>
+                <div className="bg-slate-800 rounded-3xl flex flex-col">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={fomoAlert} className="rounded-3xl"/>
+                  </div>
+                  <div className="flex flex-wrap w-full text-5xl rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">FOMO Alerts</div>
+                    <div className="text-[1rem] h-16">FOMO alerts are extremely useful and will notify you based on different market conditions surrounding trending projects.</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="bg-slate-800 rounded-3xl flex flex-col">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={followAlerts} className="rounded-3xl"/>
+                  </div>
+                  <div className="flex flex-wrap w-full text-5xl rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">Follow Alerts</div>
+                    <div className="text-[1rem] h-16">Follow alerts will show you who important figures are following and can be useful for finding new projects before they blow up.</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="bg-slate-800 rounded-3xl flex flex-col">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={contractScraper} className="rounded-3xl"/>
+                  </div>
+                  <div className="flex flex-wrap w-full text-5xl rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">Contract Alerts</div>
+                    <div className="text-[1rem] h-16">Contract alerts makes sure you have access to any lowkey mints or stealth drops that fly under the radar since not every mint is announced.</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="bg-slate-800 rounded-3xl flex flex-col">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={mintScraper} className="rounded-3xl"/>
+                  </div>
+                  <div className="flex flex-wrap w-full text-5xl rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">Mint Alerts</div>
+                    <div className="text-[1rem] h-16">Mint alerts will let you know when a new project has begun minting or when and older project gains some traction and begins to sell out.</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="bg-slate-800 rounded-3xl flex flex-col">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={raffleScraper} className="rounded-3xl"/>
+                  </div>
+                  <div className="flex flex-wrap w-full text-5xl rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">Raffle Alerts</div>
+                    <div className="text-[1rem] h-16">Raffle alerts will ensure you never miss another whitelist opportunity whether its on premint, superful, discord, twitter, you name it.</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="bg-slate-800 rounded-3xl flex flex-col">
+                  <div className="flex-grow text-5xl rounded-t-3xl m-auto">
+                    <Image src={calander} className="rounded-3xl"/>
+                  </div>
+                  <div className="flex flex-wrap w-full text-5xl rounded-b-3xl bg-slate-800 pt-6 p-8">
+                    <div className="text-[1.2rem] font-bold pb-4">Daily Calendar</div>
+                    <div className="text-[1rem] h-16">A daily list that is meant to serve as a quick guide for new projects minting from day to day and is updated regularly.</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+            <div className="m-auto">
+              <div className="hidden xs:block text-5xl text-white hover:text-gray-300 cursor-pointer" onClick={handleRightClickMobile}>
+                <FiChevronRight/>
+              </div>
+            </div>
           </div>
         </div>
     );
