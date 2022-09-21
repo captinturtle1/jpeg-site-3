0613bd9b846de99feef62a1582af2121d688c32c
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-09-20 20:24:22 -0700

    Update Landing.tsx
    added landing links

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -22,8 +22,12 @@ const Landing = () => {
           <div className="text-7xl font-bold pb-8">Into the abyss</div>
           <div className="max-w-[700px] text-2xl pb-8">A private collective of skilled traders and investors navigating the web3 space. Membership passes grant access to all community benefits while active. Each membership runs on a 30 day renewal system and can be managed at anytime via our dashboard.</div>
           <div className="max-w-[700px] text-2xl pb-8">Membership passes grant access to all community benefits while active. Each membership runs on a 30 day renewal system and can be managed at anytime via our dashboard.</div>
-          <button className="transition-all inline-block text-2xl px-6 py-3 leading-none rounded font-bold text-white bg-blue-500 hover:bg-blue-400 drop-shadow hover:drop-shadow-sm">Apply</button>
-          <button className="ml-6 transition-all inline-block text-2xl px-6 py-3 mb-3 leading-none rounded font-bold text-white bg-blue-500 hover:bg-blue-400 drop-shadow hover:drop-shadow-sm">Buy Pass</button>
+          <a>
+            <button className="transition-all inline-block text-2xl px-6 py-3 leading-none rounded font-bold text-white bg-blue-500 hover:bg-blue-400 drop-shadow hover:drop-shadow-sm">Apply</button>
+          </a>
+          <a href="https://opensea.io/collection/abyssfnf">
+            <button className="ml-6 transition-all inline-block text-2xl px-6 py-3 mb-3 leading-none rounded font-bold text-white bg-blue-500 hover:bg-blue-400 drop-shadow hover:drop-shadow-sm">Buy Pass</button>
+          </a>
         </div>
       </div>
     </div>
