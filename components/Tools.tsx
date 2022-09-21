c38a8d9275c9371f94cd30f5619b4280f5230697
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-09-20 20:19:16 -0700

    Update Tools.tsx
    added proper titles and descriptions

--- components/Tools.tsx
+++ components/Tools.tsx
@@ -39,8 +39,8 @@ const Tools = () => {
                       <Image src={fomoAlert} className="rounded-3xl"/>
                     </div>
                     <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">Lorem ipsum.</div>
-                      <div className="text-[1rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                      <div className="text-[1.2rem] font-bold pb-4">FOMO Alerts</div>
+                      <div className="text-[1rem] h-16">FOMO alerts are extremely useful and will notify you based on different market conditions surrounding trending projects.</div>
                     </div>
                   </div>
                 </SwiperSlide>
@@ -50,8 +50,8 @@ const Tools = () => {
                       <Image src={followAlerts} className="rounded-3xl"/>
                     </div>
                     <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">Lorem ipsum.</div>
-                      <div className="text-[1rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                      <div className="text-[1.2rem] font-bold pb-4">Follow Alerts</div>
+                      <div className="text-[1rem] h-16">Follow alerts will show you who important figures are following and can be useful for finding new projects before they blow up.</div>
                     </div>
                   </div>
                 </SwiperSlide>
@@ -61,8 +61,8 @@ const Tools = () => {
                       <Image src={contractScraper} className="rounded-3xl"/>
                     </div>
                     <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">Lorem ipsum.</div>
-                      <div className="text-[1rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                      <div className="text-[1.2rem] font-bold pb-4">Contract Alerts</div>
+                      <div className="text-[1rem] h-16">Contract alerts makes sure you have access to any lowkey mints or stealth drops that fly under the radar since not every mint is announced.</div>
                     </div>
                   </div>
                 </SwiperSlide>
@@ -72,8 +72,8 @@ const Tools = () => {
                       <Image src={mintScraper} className="rounded-3xl"/>
                     </div>
                     <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">Lorem ipsum.</div>
-                      <div className="text-[1rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                      <div className="text-[1.2rem] font-bold pb-4">Mint Alerts</div>
+                      <div className="text-[1rem] h-16">Mint alerts will let you know when a new project has begun minting or when and older project gains some traction and begins to sell out.</div>
                     </div>
                   </div>
                 </SwiperSlide>
@@ -83,8 +83,8 @@ const Tools = () => {
                       <Image src={raffleScraper} className="rounded-3xl"/>
                     </div>
                     <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">Lorem ipsum.</div>
-                      <div className="text-[1rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                      <div className="text-[1.2rem] font-bold pb-4">Raffle Alerts</div>
+                      <div className="text-[1rem] h-16">Raffle alerts will ensure you never miss another whitelist opportunity whether its on premint, superful, discord, twitter, you name it.</div>
                     </div>
                   </div>
                 </SwiperSlide>
@@ -94,8 +94,8 @@ const Tools = () => {
                       <Image src={calander} className="rounded-3xl"/>
                     </div>
                     <div className="flex flex-wrap w-full text-5xl bg-400-800 rounded-b-3xl bg-slate-800 pt-6 p-8">
-                      <div className="text-[1.2rem] font-bold pb-4">Lorem ipsum.</div>
-                      <div className="text-[1rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                      <div className="text-[1.2rem] font-bold pb-4">Daily Calendar</div>
+                      <div className="text-[1rem] h-16">A daily list that is meant to serve as a quick guide for new projects minting from day to day and is updated regularly.</div>
                     </div>
                   </div>
                 </SwiperSlide>
