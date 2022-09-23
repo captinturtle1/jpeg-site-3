09eb30750d662e5a9b396cecc908e617195a0d04
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-09-22 23:36:18 -0700

    Update Landing.tsx
    made landing page look good on smaller screens

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -9,13 +9,13 @@ const Landing = () => {
       <div className="bottom-0 absolute z-[2] translate-y-[6.9px]">
         <Image quality={100} src={bg}/>
       </div>
-      <div className="z-[4] grid grid-cols-1 h-[800px] lg:h-[900px] lg:grid-cols-2 gap-0 lg:gap-20">
-        <div className="mx-auto mt-20 xs:mt-32 lg:m-auto w-[200px] xs:w-[300px] lg:w-[650px] flex justify-center drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]">
+      <div className="z-[4] grid grid-cols-1 h-[900px] lg:grid-cols-2 gap-0 lg:gap-20">
+        <div className="mx-auto mt-20 xs:mt-32 lg:m-auto w-[200px] xs:w-[300px] sm:w-[400px] md:w-[400px] lg:w-[580px] xl:w-[650px] flex justify-center drop-shadow-[15px_10px_10px_rgba(0,0,0,0.4)]">
           <div>
             <Image src={squid}/>
           </div>
         </div>
-        <div className="m-auto px-10 lg:p-0 flex flex-wrap">
+        <div className="m-auto px-10 lg:p-0 flex flex-col">
           <div className="m-auto lg:m-0 text-4xl lg:text-7xl text-center lg:text-left font-bold pb-8">Into the abyss</div>
           <div className="max-w-[700px] text-center lg:text-left text-md xs:text-lg lg:text-2xl pb-8">The Abyss community is hand selected by our staff to ensure the best for each and every member. Owning a membership pass grants access to a wide range of information from exclusive curated alpha that is only available in house, to some of the best tools on the market.</div>
           <div className="hidden lg:block max-w-[700px] text-center lg:text-left text-md xs:text-lg lg:text-2xl pb-8">To join you can apply when new submissions open up or purchase a pass on OpenSea. Each pass has a renewal of either 30 or 45 days and can be managed using our dashboard.</div>
