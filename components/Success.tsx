e6a2fff75ebdd6a2e9f84f83873b07730a10e095
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-09-26 19:53:05 -0700

    success page
    added info to cards

--- components/Success.tsx
+++ components/Success.tsx
@@ -4,11 +4,11 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image'
 import bg from '../public/bg2.png'
 
-import azuki from '../public/azuki.jpg'
-import CloneX from '../public/CloneX.png'
-import doodles from '../public/doodles.jpg'
-import moonbirds from '../public/moonbirds.png'
-import parallel from '../public/parallel.png'
+import officehours from '../public/officehours.png'
+import eightliens from '../public/8liens.png'
+import pudgypenguins from '../public/pudgypenguins.png'
+import isekai from '../public/isekai.png'
+import lilpudgys from '../public/lilpudgys.png'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCards } from "swiper";
@@ -30,61 +30,66 @@ const Success = () => {
             >
               <SwiperSlide>
                 <div className="flex flex-col">
-                  <Image src={azuki} className="rounded-t-3xl"/>
-                  <div className="grid gap-y-6 bg-slate-800 rounded-b-3xl pb-12">
-                    <h1 className="my-auto text-4xl font-bold m-auto col-span-2 pt-6">Azuki</h1>
-                    <h1 className="text-2xl ml-6 lg:ml-16">Buy in:</h1>
-                    <h1 className="text-2xl ml-6">10 E</h1>
-                    <h1 className="text-2xl ml-6 lg:ml-16">Sell:</h1>
-                    <h1 className="text-2xl ml-6">1,000 E</h1>
+                  <Image layout="responsive" src={officehours} className="rounded-t-3xl"/>
+                  <div className="grid gap-y-6 bg-slate-800 rounded-b-3xl pb-12 translate-y-[-1px]">
+                    <h1 className="my-auto text-4xl font-bold m-auto col-span-2 pt-6">Office Hours</h1>
+                    <h1 className="text-2xl ml-6 lg:ml-16">Avg Entry:</h1>
+                    <h1 className="text-2xl ml-6">0 Ξ</h1>
+                    <h1 className="text-2xl ml-6 lg:ml-16">Avg Exit</h1>
+                    <h1 className="text-2xl ml-6">0.2 Ξ</h1>
+                    <h1 className="text-2xl font-bold col-span-2 m-auto">2000% ROI</h1>
                   </div>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="flex flex-col">
-                  <Image src={CloneX} className="rounded-t-3xl"/>
-                  <div className="grid gap-y-6 bg-slate-800 rounded-b-3xl pb-12">
-                    <h1 className="my-auto text-4xl font-bold m-auto col-span-2 pt-6">Azuki</h1>
-                    <h1 className="text-2xl ml-6 lg:ml-16">Buy in:</h1>
-                    <h1 className="text-2xl ml-6">10 E</h1>
-                    <h1 className="text-2xl ml-6 lg:ml-16">Sell:</h1>
-                    <h1 className="text-2xl ml-6">1,000 E</h1>
+                  <Image layout="responsive" src={eightliens} className="rounded-t-3xl"/>
+                  <div className="grid gap-y-6 bg-slate-800 rounded-b-3xl pb-12 translate-y-[-1px]">
+                    <h1 className="my-auto text-4xl font-bold m-auto col-span-2 pt-6">8liens</h1>
+                    <h1 className="text-2xl ml-6 lg:ml-16">Avg Entry:</h1>
+                    <h1 className="text-2xl ml-6">0.1 Ξ</h1>
+                    <h1 className="text-2xl ml-6 lg:ml-16">Avg Exit</h1>
+                    <h1 className="text-2xl ml-6">0.4 Ξ</h1>
+                    <h1 className="text-2xl font-bold col-span-2 m-auto">300% ROI</h1>
                   </div>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="flex flex-col">
-                  <Image src={doodles} className="rounded-t-3xl"/>
-                  <div className="grid gap-y-6 bg-slate-800 rounded-b-3xl pb-12">
-                    <h1 className="my-auto text-4xl font-bold m-auto col-span-2 pt-6">Azuki</h1>
-                    <h1 className="text-2xl ml-6 lg:ml-16">Buy in:</h1>
-                    <h1 className="text-2xl ml-6">10 E</h1>
-                    <h1 className="text-2xl ml-6 lg:ml-16">Sell:</h1>
-                    <h1 className="text-2xl ml-6">1,000 E</h1>
+                  <Image layout="responsive" src={pudgypenguins} className="rounded-t-3xl"/>
+                  <div className="grid gap-y-6 bg-slate-800 rounded-b-3xl pb-12 translate-y-[-1px]">
+                    <h1 className="my-auto text-4xl font-bold m-auto col-span-2 pt-6">Pudgy Penguins</h1>
+                    <h1 className="text-2xl ml-6 lg:ml-16">Avg Entry:</h1>
+                    <h1 className="text-2xl ml-6">1.1 Ξ</h1>
+                    <h1 className="text-2xl ml-6 lg:ml-16">Avg Exit</h1>
+                    <h1 className="text-2xl ml-6">3.7 Ξ</h1>
+                    <h1 className="text-2xl font-bold col-span-2 m-auto">236% ROI</h1>
                   </div>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="flex flex-col">
-                  <Image src={moonbirds} className="rounded-t-3xl"/>
-                  <div className="grid gap-y-6 bg-slate-800 rounded-b-3xl pb-12">
-                    <h1 className="my-auto text-4xl font-bold m-auto col-span-2 pt-6">Azuki</h1>
-                    <h1 className="text-2xl ml-6 lg:ml-16">Buy in:</h1>
-                    <h1 className="text-2xl ml-6">10 E</h1>
-                    <h1 className="text-2xl ml-6 lg:ml-16">Sell:</h1>
-                    <h1 className="text-2xl ml-6">1,000 E</h1>
+                  <Image layout="responsive" src={isekai} className="rounded-t-3xl"/>
+                  <div className="grid gap-y-6 bg-slate-800 rounded-b-3xl pb-12 translate-y-[-1px]">
+                    <h1 className="my-auto text-4xl font-bold m-auto col-span-2 pt-6">Isekai</h1>
+                    <h1 className="text-2xl ml-6 lg:ml-16">Avg Entry:</h1>
+                    <h1 className="text-2xl ml-6">0.15 Ξ</h1>
+                    <h1 className="text-2xl ml-6 lg:ml-16">Avg Exit</h1>
+                    <h1 className="text-2xl ml-6">0.5 Ξ</h1>
+                    <h1 className="text-2xl font-bold col-span-2 m-auto">233% ROI</h1>
                   </div>
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="flex flex-col">
-                  <Image src={parallel} className="rounded-t-3xl"/>
-                  <div className="grid gap-y-6 bg-slate-800 rounded-b-3xl pb-12">
-                    <h1 className="my-auto text-4xl font-bold m-auto col-span-2 pt-6">Azuki</h1>
-                    <h1 className="text-2xl ml-6 lg:ml-16">Buy in:</h1>
-                    <h1 className="text-2xl ml-6">10 E</h1>
-                    <h1 className="text-2xl ml-6 lg:ml-16">Sell:</h1>
-                    <h1 className="text-2xl ml-6">1,000 E</h1>
+                  <Image layout="responsive" src={lilpudgys} className="rounded-t-3xl"/>
+                  <div className="grid gap-y-6 bg-slate-800 rounded-b-3xl pb-12 translate-y-[-1px]">
+                    <h1 className="my-auto text-4xl font-bold m-auto col-span-2 pt-6">Lil Pudgys</h1>
+                    <h1 className="text-2xl ml-6 lg:ml-16">Avg Entry:</h1>
+                    <h1 className="text-2xl ml-6">0.08 Ξ</h1>
+                    <h1 className="text-2xl ml-6 lg:ml-16">Avg Exit</h1>
+                    <h1 className="text-2xl ml-6">0.25 Ξ</h1>
+                    <h1 className="text-2xl font-bold col-span-2 m-auto">212% ROI</h1>
                   </div>
                 </div>
               </SwiperSlide>
