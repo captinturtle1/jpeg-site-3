73a995ba6e3712842b1304deecd6c26e5d94c1ed
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-10-21 00:02:59 -0700

    changed metadata api

--- pages/api/expired/[...expired].ts
+++ pages/api/expired/[...expired].ts
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     try {
-      res.redirect(307, "https://ipfs.io/ipfs/Qme5QHNUjUiZbao7Si54uveTJtUZJ3gPgVbUSJet1h4ybY/Expired.gif")
+      res.redirect(307, "https://abyssnftdata.s3.us-east-2.amazonaws.com/Closed.mp4")
     } catch (err) {
       res.status(500).send({ error: 'failed to fetch data' })
     }
--- pages/api/expiredGif/[...expiredGif].ts
+++ pages/api/expiredGif/[...expiredGif].ts
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     try {
-      res.redirect(307, "https://abyssnftdata.s3.us-east-2.amazonaws.com/Open.mp4")
+      res.redirect(307, "https://abyssnftdata.s3.us-east-2.amazonaws.com/closedgif.gif")
     } catch (err) {
       res.status(500).send({ error: 'failed to fetch data' })
     }
