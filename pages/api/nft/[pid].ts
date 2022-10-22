e8f27e81d64b013d8e107b553608c0089bb7b25a
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-10-21 20:46:20 -0700

    nft api bug fix
    remove the extra object layer in the response

--- pages/api/nft/[pid].ts
+++ pages/api/nft/[pid].ts
@@ -81,7 +81,7 @@ export default async function handler(req, res) {
     }
 
     try {
-        res.status(200).json({ nftObject })
+        res.status(200).json( nftObject )
     } catch (err) {
         res.status(500).json({ error: 'failed to load data' })
     } 
